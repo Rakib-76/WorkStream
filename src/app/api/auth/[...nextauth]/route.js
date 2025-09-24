2364e19b31e41fa387a9e55299f01700b47f9a90
Rakib-76 <mrrakibhasan76@gmail.com>
2025-09-25 00:33:21 +0600

    add the nothing

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,8 +2,9 @@ import { loginUser } from "../../../actions/auth/loginUser"
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
 
+
 export const authOptions = {
-     secret: process.env.NEXTAUTH_SECRET, 
+    secret: process.env.NEXTAUTH_SECRET,
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
@@ -19,24 +20,26 @@ export const authOptions = {
             },
             async authorize(credentials) {
                 // console.log(credentials);
-               
+
                 const user = await loginUser(credentials)
 
                 // If no error and we have user data, return it
-                if ( user) {
+                if (user) {
                     return user
                 }
                 // Return null if user data could not be retrieved
                 return null
             }
-        })
+        }),
+
     ],
     pages: {
         signIn: '/logIn',
-      
+
     }
 }
 
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
+
