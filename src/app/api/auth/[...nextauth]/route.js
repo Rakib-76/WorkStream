b0b59bb258a8fb3c291ae620de115040f3f89218
Rakib-76 <mrrakibhasan76@gmail.com>
2025-09-25 12:44:57 +0600

    add the fix bug from login validation

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -8,12 +8,8 @@ export const authOptions = {
     // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
-            // The name to display on the sign in form (e.g. 'Sign in with...')
+
             name: 'Credentials',
-            // The credentials is used to generate a suitable form on the sign in page.
-            // You can specify whatever fields you are expecting to be submitted.
-            // e.g. domain, username, password, 2FA token, etc.
-            // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
                 username: { label: "Email", type: "text", placeholder: "Enter email" },
                 password: { label: "Password", type: "password" }
@@ -24,7 +20,7 @@ export const authOptions = {
                 const user = await loginUser(credentials)
 
                 // If no error and we have user data, return it
-                if (user) {
+                if (user ) {
                     return user
                 }
                 // Return null if user data could not be retrieved
