68a2b5cfe63220a78f86fc369746bb97cee0a201
Rakib-76 <mrrakibhasan76@gmail.com>
2025-09-30 00:31:56 +0600

    add to wrapping by sespense

--- src/app/(auth_page)/login/page.jsx
+++ src/app/(auth_page)/login/page.jsx
@@ -1,12 +1,14 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import LoginForm from "./component/LoginForm";
 
 export default function LoginPage() {
 
   return (
     <div className="card  w-full max-w-sm shrink-0 shadow-2xl">
-      <LoginForm/>
+      <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
     </div>
   );
 }
