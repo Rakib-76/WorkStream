da85f62620ab8c6a2c9c1de60f10c3b23ccf605c
syed bayzid <husains82849@gmail.com>
2025-10-01 08:34:39 +0600

    import issue solved

--- src/app/(auth_page)/register/page.jsx
+++ src/app/(auth_page)/register/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import RegisterForm from "./component/RegisterForm";
+import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
   const googleSignIn = () => {
