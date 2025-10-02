f4eaec99a372c42b0f29eb6a71d6ae498cde889b
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-10-02 23:55:36 +0600

    card section update

--- src/app/Components/(home_page)/UI/card.jsx
+++ src/app/Components/(home_page)/UI/card.jsx
@@ -15,7 +15,7 @@ const Card = React.forwardRef(({ className, ...props }, ref) => (
 Card.displayName = "Card";
 
 const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
+  <div ref={ref} className={cn("flex flex-col space-y-1.5 ", className)} {...props} />
 ));
 CardHeader.displayName = "CardHeader";
 
@@ -34,12 +34,12 @@ const CardDescription = React.forwardRef(({ className, ...props }, ref) => (
 CardDescription.displayName = "CardDescription";
 
 const CardContent = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn(" pt-0", className)} {...props} />
 ));
 CardContent.displayName = "CardContent";
 
 const CardFooter = React.forwardRef(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("flex items-center  pt-0", className)} {...props} />
 ));
 CardFooter.displayName = "CardFooter";
 
