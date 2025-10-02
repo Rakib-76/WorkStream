c1ee27a747a3587c6ba9022c761875e04b7b7066
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-10-02 23:56:27 +0600

    team section update

--- src/app/Components/(dashboard_page)/Team/Team.jsx
+++ src/app/Components/(dashboard_page)/Team/Team.jsx
@@ -41,7 +41,7 @@ export default function Team() {
   };
 
   return (
-    <section className="py-8 px-4 max-w-7xl mx-auto">
+    <section className="">
       {/* Header + Add Member Btn */}
       <div className="flex items-center justify-between mb-6">
         <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-100">Team Details</h2>
