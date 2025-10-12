6d5a90c4d3be90727a431ff63541adbf763f77e4
Rakib-76 <mrrakibhasan76@gmail.com>
2025-10-12 11:26:05 +0600

    add to fix import problem from lib

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { dbConnect } from "../../../lib/dbConnect"; // adjust path when you copy
+import dbConnect  from "../../../lib/dbConnect"; 
 
 
 export async function GET(request) {
