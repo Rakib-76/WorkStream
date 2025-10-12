02cdd7568dbf641364af73654c22cb77d9aa926c
Rakib-76 <mrrakibhasan76@gmail.com>
2025-10-13 00:36:33 +0600

    add to fix deployment issue

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,7 +1,7 @@
 
 import { NextResponse } from "next/server";
 import dbConnect  from "../../../lib/dbConnect";
-import dbConnect  from "../../../lib/dbConnect"; 
+
 
 
 export async function GET(request) {
