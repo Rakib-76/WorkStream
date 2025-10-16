a7cda2f60f888b06c75b78300d14098fdbee4f63
Ashfaq Sarker Abid <abidsarker213@gmail.com>
2025-10-16 20:49:22 +0600

    margin padding  update

--- src/app/Components/(dashboard_page)/Sidebar/Sidebar.jsx
+++ src/app/Components/(dashboard_page)/Sidebar/Sidebar.jsx
@@ -106,7 +106,7 @@ export default function Sidebar({ activeItem, setActiveItem }) {
         }`}
       >
         <h1 className="text-lg font-bold text-foreground truncate">
-          {selectedProject ? selectedProject.projectName : "No Project Selected"}
+          Project : {selectedProject ? selectedProject.projectName : "No Project Selected"}
         </h1>
       </div>
 
@@ -162,10 +162,10 @@ export default function Sidebar({ activeItem, setActiveItem }) {
                     <li
                       key={child.name}
                       onClick={() => setActiveItem(child.name)}
-                      className={`flex items-center gap-3 cursor-pointer rounded-lg px-3 py-2 transition
+                      className={`flex  items-center gap-3 cursor-pointer rounded-br-4xl rounded-tr-4xl mr-16 px-3 py-2 transition
                         ${
                           activeItem === child.name
-                            ? "bg-gradient-to-r from-[var(--primary)] to-[var(--secondary)] text-[var(--primary-foreground)] font-semibold shadow-md"
+                            ? "bg-gradient-to-r from-[var(--primary)] to-[var(--secondary)/40] text-[var(--primary-foreground)] font-semibold shadow-md"
                             : "hover:bg-[var(--sidebar-accent)] hover:text-[var(--sidebar-accent-foreground)]"
                         }`}
                     >
